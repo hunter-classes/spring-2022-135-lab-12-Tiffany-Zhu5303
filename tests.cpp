3a62bf620ef42b19cca1392afeb7283a514bd5db
Tiffany-Zhu5303 <Tiffany2016d@gmail.com>
2022-04-26 19:58:03 -0400

    Task B tests

--- tests.cpp
+++ tests.cpp
@@ -16,3 +16,15 @@ TEST_CASE("making vectors"){
   CHECK(second.back() == 19);
   CHECK(second.size() == 20);
 }
+
+TEST_CASE("vector of positive integers only"){
+  std::vector<int> third = {0, 1, -2, 3, -4, -2, -4, 6, -7};
+  std::vector<int> positiveThird = goodVibes(third);
+
+  for(int i = 0; i < positiveThird.size(); i++){
+    CHECK(positiveThird[i] > 0);
+  }
+
+  CHECK(positiveThird.front() == 1);
+  CHECK(positiveThird.back() == 6);
+}
